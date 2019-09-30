26cbd6518c881e03a2a6cb0b48f228ea8000ed9f
venkat prudhvi Dommaraju <S537236@nwmissouri.edu>
2019-09-30 11:22:23 -0500

    done changes

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 function largest()
 {
-const guest=document.getElementById("guest").value;
  const str1=document.getElementById("str1").value;
  const str2=document.getElementById("str2").value;
  const long=Math.max(str1.length,str2.length);
- document.getElementById("result").innerHTML=guest+", Longest length is:"+long;
+ document.getElementById("result").innerHTML="The longest length is:"+long;
 
 }
 document.getElementById("addButton").addEventListener('click',largest);
